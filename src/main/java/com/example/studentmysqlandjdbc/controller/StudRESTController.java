29baebf14b506a145a633cbed2c4a0d87835be53
CodeClod <mika9103@stud.kea.dk>
2023-02-10 09:57:50 +0100

    More HTTP mapping added

--- src/main/java/com/example/studentmysqlandjdbc/controller/StudRESTController.java
+++ src/main/java/com/example/studentmysqlandjdbc/controller/StudRESTController.java
@@ -5,9 +5,7 @@ import com.example.studentmysqlandjdbc.repositories.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -56,6 +54,19 @@ public class StudRESTController {
     }
 
 
+    @PostMapping("/student")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Student postStudent(@RequestBody Student student) {
+        System.out.println(student.getName());
+        return  studentRepository.save(student);
+
+    }
+
+    @DeleteMapping("/deleteStudent/{id}")
+    public ResponseEntity<Student> deleteStudent(@PathVariable Integer id) {
+         studentRepository.deleteById(id);
+         return new ResponseEntity<>(HttpStatus.OK);
+    }
 
 
 }
